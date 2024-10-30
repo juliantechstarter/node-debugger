8a9915214342d45c12521014cf3b7ec41bdc5eb4
juliantechstarter <julian.saenger@edu.techstarter.de>
2024-10-30 12:12:56 +0100

    ghgh

--- index.js
+++ index.js
@@ -2,18 +2,16 @@
 var items = [1, "2", 3, "four", 5, null, undefined, 6]
 const results = []
 
-items.foreach((item) => {
-    if(typeof item === 'number')
-        if(item % 2 === 0) results.push(item* 2)
-
-    else if(item === null)
-    console.log("Found a null item")
-
-    else 
-    {
-        result.push(parseInt(item))
+items.forEach((item) => {
+    if (typeof item === 'number') {
+        if (item % 2 === 0) results.push(item * 2);
+    } else if (item === null) {
+        console.log("Found a null item");
+    } else {
+        results.push(parseInt(item));
     }
 });
+
 
 console.log("Original items:", items)
 console.log("Filtered and transformed items:", results)
